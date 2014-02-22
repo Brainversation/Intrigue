bf621ce18f91c2a258d7fc0c3a92a47db1e06d1c
Kayvan Boudai <kboudai@ucsc.edu>
2014-02-21 22:36:07 -0800

    added pop to hotspot

--- Intrigue/Assets/Scripts/AI/HotSpot.cs
+++ Intrigue/Assets/Scripts/AI/HotSpot.cs
@@ -2,13 +2,20 @@
 using System.Collections;
 
 public class HotSpot : MonoBehaviour {
+
+	[HideInInspector] public int population = 0;
+
 	void OnTriggerEnter(Collider other){
-		if(other.tag == "Guest")
+		if(other.tag == "Guest"){
 			other.gameObject.GetComponent<BaseAI>().atDrink = true;
+			++population;
+		}
 	}
 
 	void OnTriggerExit(Collider other){
-		if(other.tag == "Guest")
+		if(other.tag == "Guest"){
 			other.gameObject.GetComponent<BaseAI>().atDrink = false;
+			--population;
+		}
 	}
 }
