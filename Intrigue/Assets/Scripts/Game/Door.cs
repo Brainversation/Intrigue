f54e76bcd1d08740140804e1d129b21c432d74c3
Ian Winter <iwinter@ucsc.edu>
2014-03-10 14:32:51 -0700

    Initial stages of animated doors, mostly working on front entrance

---
+++ Intrigue/Assets/Scripts/Game/Door.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public bool open = false;
+	private float speed = 8F;
+	private int curInside = 0;
+	private Vector3 startMarker;
+	private float moveDist = 8.1f;
+	private Vector3 temp;
+
+    void Start() {
+        startMarker = transform.localPosition;
+    }
+    void Update() {
+    	if(curInside>=1){
+    		open = true;
+    		if(transform.localPosition.x<startMarker.x+moveDist){
+    			transform.Translate(Time.deltaTime*speed, 0f, 0f, Space.Self);
+    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center - new Vector3(Time.deltaTime*speed*2,0f,0f);
+    		}
+		}
+		else {
+			if(transform.localPosition.x>startMarker.x){
+    			transform.Translate(Time.deltaTime*-speed, 0f, 0f, Space.Self);
+    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center + new Vector3(Time.deltaTime*speed*2,0f,0f);
+    		}
+    	}
+    }
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Spy")||other.gameObject.CompareTag("Guard")||other.gameObject.CompareTag("Guest")){
+			curInside++;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.CompareTag("Spy")||other.gameObject.CompareTag("Guard")||other.gameObject.CompareTag("Guest")){
+			curInside--;
+		}
+	}
+}
