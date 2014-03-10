f3d6fd998436eea32823af71303e31b542635701
Ian Winter <iwinter@ucsc.edu>
2014-03-10 15:50:48 -0700

    Fixed animated doors

--- Intrigue/Assets/Scripts/Game/Door.cs
+++ Intrigue/Assets/Scripts/Game/Door.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Door : MonoBehaviour {
 
 	public bool open = false;
+	public bool openRight = false;
 	private float speed = 8F;
 	private int curInside = 0;
 	private Vector3 startMarker;
@@ -11,22 +12,41 @@ public class Door : MonoBehaviour {
 	private Vector3 temp;
 
     void Start() {
-        startMarker = transform.localPosition;
+        startMarker = transform.position;
     }
     void Update() {
-    	if(curInside>=1){
-    		open = true;
-    		if(transform.localPosition.x<startMarker.x+moveDist){
-    			transform.Translate(Time.deltaTime*speed, 0f, 0f, Space.Self);
-    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center - new Vector3(Time.deltaTime*speed*2,0f,0f);
-    		}
-		}
-		else {
-			if(transform.localPosition.x>startMarker.x){
-    			transform.Translate(Time.deltaTime*-speed, 0f, 0f, Space.Self);
-    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center + new Vector3(Time.deltaTime*speed*2,0f,0f);
-    		}
-    	}
+    	if(openRight){
+	    	if(curInside>=1){
+	    		open = true;
+	    		if(transform.position.x<startMarker.x+moveDist){
+	    			transform.Translate(Time.deltaTime*speed, 0f, 0f, Space.Self);
+	    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center - new Vector3(Time.deltaTime*speed*2,0f,0f);
+	    		}
+			}
+			else {
+				open = false;
+				if(transform.position.x>startMarker.x){
+	    			transform.Translate(Time.deltaTime*-speed, 0f, 0f, Space.Self);
+	    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center + new Vector3(Time.deltaTime*speed*2,0f,0f);
+	    		}
+	    	}
+	    }
+	    else{
+	    	if(curInside>=1){
+	    		open = true;
+	    		if(transform.position.x>startMarker.x-moveDist){
+	    			transform.Translate(Time.deltaTime*speed, 0f, 0f, Space.Self);
+	    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center - new Vector3(Time.deltaTime*speed*2,0f,0f);
+	    		}
+			}
+			else {
+				open = false;
+				if(transform.position.x<startMarker.x){
+	    			transform.Translate(Time.deltaTime*-speed, 0f, 0f, Space.Self);
+	    			GetComponent<SphereCollider>().center = GetComponent<SphereCollider>().center + new Vector3(Time.deltaTime*speed*2,0f,0f);
+	    		}
+	    	}
+	    }
     }
 
 	void OnTriggerEnter(Collider other){
