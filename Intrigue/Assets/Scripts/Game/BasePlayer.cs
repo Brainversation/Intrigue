7c0d5b70fb7840cb63c8370b8934f594ee033282
Kayvan Boudai <Kayvan.Boudai@gmail.com>
2014-05-02 17:22:02 -0700

    Hopefully fixed it

--- Intrigue/Assets/Scripts/Game/BasePlayer.cs
+++ Intrigue/Assets/Scripts/Game/BasePlayer.cs
@@ -192,7 +192,7 @@ public class BasePlayer : MonoBehaviour {
 		BasePlayer bp;
 		foreach(GameObject playerInstance in allPlayers){
 			bp = playerInstance.GetComponent<BasePlayer>();
-			bp.GetComponent<PhotonView>().RPC("receiveMessage", PhotonNetwork.player.Find(bp.photonID), eventMessage);
+			bp.GetComponent<PhotonView>().RPC("receiveMessage", PhotonPlayer.Find(bp.photonID), eventMessage);
 		}
 	}
 
