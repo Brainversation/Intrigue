a20e55f2847bd0009e716a5863892c2671a6facd
Ian Winter <iwinter@ucsc.edu>
2014-05-18 16:30:24 -0700

    added server indicators

---
+++ Intrigue/Assets/Scripts/Game/ServerIndicator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerIndicator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Camera.main!=null){
+			inView();
+			transform.LookAt(Camera.main.transform);
+			transform.Rotate(Vector3.up, 180);
+		}
+	}
+
+	void inView(){
+		if(Vector3.Distance(Camera.main.transform.position, transform.position)<70){
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+		}
+		else{
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+}
