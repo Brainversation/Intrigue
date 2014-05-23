64b1c8780f110eb8b2809bcecc746f147ba75fbf
Brian Aston <box1290@gmail.com>
2014-05-22 21:29:17 -0700

    Added animations to the Hologram Boss.

---
+++ Intrigue/Assets/AnimateBoss.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateBoss : MonoBehaviour {
+
+	private bool done = true;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(done){
+			done = false;
+			StartCoroutine(StartPointing(Random.Range(5,10)));
+		}
+	}
+
+	IEnumerator StartPointing(float time){
+		yield return new WaitForSeconds(time);
+		anim.SetBool("Pointing", true);
+		StartCoroutine(EndPointing(anim.GetCurrentAnimatorStateInfo(0).length));
+	}
+
+	IEnumerator EndPointing(float time){
+		yield return new WaitForSeconds(time);
+		anim.SetBool("Pointing", false);
+		done = true;
+	}
+}
