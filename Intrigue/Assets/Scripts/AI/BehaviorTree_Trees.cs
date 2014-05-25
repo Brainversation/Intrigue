e3193ae17eb6dee28d28b364a09cabb4fc771aa4
Kayvan Boudai <Kayvan.Boudai@gmail.com>
2014-05-25 15:03:23 -0700

    Added a bunch of idle selectors to the AI

--- Intrigue/Assets/Scripts/AI/BehaviorTree_Trees.cs
+++ Intrigue/Assets/Scripts/AI/BehaviorTree_Trees.cs
@@ -29,6 +29,14 @@ namespace BehaviorTree{
 		public IdleSelector(){
 			this.addChild(new IdleSad());
 			this.addChild(new IdleHover());
+			this.addChild(new IdleComeHere());
+			this.addChild(new IdleWave());
+			this.addChild(new IdleCough());
+			this.addChild(new HealClick());
+			this.addChild(new Yawn());
+			this.addChild(new FacePalm());
+			this.addChild(new IdleSadHips());
+			this.addChild(new BackScratch());
 			rand();
 		}
 	}
@@ -36,7 +44,7 @@ namespace BehaviorTree{
 	class IdleSad : Sequence {
 		public IdleSad(){
 			this.addChild(new AnimStart("IdleSad"));
-			this.addChild(new Wait(5));
+			this.addChild(new Wait(6));
 			this.addChild(new AnimStop("IdleSad"));
 		}
 	}
@@ -46,6 +54,70 @@ namespace BehaviorTree{
 			this.addChild(new AnimStart("IdleHover"));
 			this.addChild(new Wait(7));
 			this.addChild(new AnimStop("IdleHover"));
+		}
+	}
+
+	class IdleComeHere : Sequence {
+		public IdleComeHere(){
+			this.addChild(new AnimStart("IdleComeHere"));
+			this.addChild(new Wait(4));
+			this.addChild(new AnimStop("IdleComeHere"));
+		}
+	}
+
+	class IdleWave : Sequence {
+		public IdleWave(){
+			this.addChild(new AnimStart("IdleWave"));
+			this.addChild(new Wait(5));
+			this.addChild(new AnimStop("IdleWave"));
+		}
+	}
+
+	class IdleCough : Sequence {
+		public IdleCough(){
+			this.addChild(new AnimStart("IdleCough"));
+			this.addChild(new Wait(5));
+			this.addChild(new AnimStop("IdleCough"));
+		}
+	}
+
+	class HealClick : Sequence {
+		public HealClick(){
+			this.addChild(new AnimStart("HealClick"));
+			this.addChild(new Wait(3));
+			this.addChild(new AnimStop("HealClick"));
+		}
+	}
+
+	class Yawn : Sequence {
+		public Yawn(){
+			this.addChild(new AnimStart("Yawn"));
+			this.addChild(new Wait(4));
+			this.addChild(new AnimStop("Yawn"));
+		}
+	}
+
+	class FacePalm : Sequence {
+		public FacePalm(){
+			this.addChild(new AnimStart("FacePalm"));
+			this.addChild(new Wait(4));
+			this.addChild(new AnimStop("FacePalm"));
+		}
+	}
+
+	class IdleSadHips : Sequence {
+		public IdleSadHips(){
+			this.addChild(new AnimStart("IdleSadHips"));
+			this.addChild(new Wait(5));
+			this.addChild(new AnimStop("IdleSadHips"));
+		}
+	}
+
+	class BackScratch : Sequence {
+		public BackScratch(){
+			this.addChild(new AnimStart("BackScratch"));
+			this.addChild(new Wait(5));
+			this.addChild(new AnimStop("BackScratch"));
 		}
 	}
 
