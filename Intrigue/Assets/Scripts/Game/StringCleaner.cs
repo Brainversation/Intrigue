916369d944fa020833bf4172d1972ce929e6e4b7
Ian Winter <iwinter@ucsc.edu>
2014-05-31 14:36:37 -0700

    improvements to Family Settings

--- Intrigue/Assets/Scripts/Game/StringCleaner.cs
+++ Intrigue/Assets/Scripts/Game/StringCleaner.cs
@@ -13,118 +13,29 @@
  **************************************************************************/
 
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using Hashtable = ExitGames.Client.Photon.Hashtable;
+using System.Text.RegularExpressions;
 
 public class StringCleaner : MonoBehaviour {
 
-	private static List<string> naughtyWords = new List<string>();
-	private static List<string> goodWords = new List<string>();
+	private static string PatternTemplate;
+	private static RegexOptions Options;
+	private static Regex rx;
 
- 	static string ConvertStringArrayToString(string[] array)
-		{
-		StringBuilder builder = new StringBuilder();
-		foreach (string value in array)
-		{
-		    builder.Append(value);
-		    builder.Append(' ');
-		}
-		return builder.ToString();
+  	static string ReplaceWords(Match m){
+
+		string [] robotWords = new [] { "BEEP", "BOOP", "BOP", "ZIP", "ZAP", "ZOOP", "SQUEEBOP"};
+		return robotWords[Random.Range(0,7)];
     }
 
-	public static string CleanString(string checkStr)
-    {
-    	//createStringLists();
-        string[] inStrArray = checkStr.Split(new char[] {' '});
-        // LOOP THROUGH WORDS IN MESSAGE
-        for (int x = 0; x < inStrArray.Length; x++)
-        {
-            // LOOP THROUGH PROFANITY WORDS
-            for (int i = 0; i < naughtyWords.Count; i++)
-            {
-                if( inStrArray[x] == naughtyWords[i] ||  inStrArray[x].ToUpper() == naughtyWords[i].ToUpper()){
-					inStrArray[x] = goodWords[i];
-                }
-                else if(inStrArray[x] == naughtyWords[i] +"s" || inStrArray[x] == naughtyWords[i] +"es" || inStrArray[x].ToUpper() == naughtyWords[i].ToUpper() +"S" || inStrArray[x].ToUpper() == naughtyWords[i].ToUpper() +"ES"){	
-					inStrArray[x] = goodWords[i] + "s";
-				}
-            }
+    public static string CleanString(string checkStr){
 
-        }
-        string cleanedString = ConvertStringArrayToString(inStrArray);
-        return cleanedString;
+		string result = rx.Replace(checkStr, new MatchEvaluator(ReplaceWords));
+		return result;
     }
 
    public static void createStringLists(){
-		naughtyWords.Add("Shit");
-		goodWords.Add("Poop");
-		naughtyWords.Add("shit");
-		goodWords.Add("poop");
-		naughtyWords.Add("Ass");
-		goodWords.Add("Booty");
-		naughtyWords.Add("ass");
-		goodWords.Add("booty");
-		naughtyWords.Add("Fuck");
-		goodWords.Add("Fudge");
-		naughtyWords.Add("fuck");
-		goodWords.Add("fudge");
-		naughtyWords.Add("Bitch");
-		goodWords.Add("Meany Face");
-		naughtyWords.Add("bitch");
-		goodWords.Add("meany face");
-		naughtyWords.Add("Damn");
-		goodWords.Add("Darn");
-		naughtyWords.Add("damn");
-		goodWords.Add("darn");
-		naughtyWords.Add("Cunt");
-		goodWords.Add("Cheese Doodle");
-		naughtyWords.Add("cunt");
-		goodWords.Add("cheese doodle");
-		naughtyWords.Add("Penis");
-		goodWords.Add("Pee Pee");
-		naughtyWords.Add("penis");
-		goodWords.Add("pee pee");
-		naughtyWords.Add("Vagina");
-		goodWords.Add("Taco");
-		naughtyWords.Add("vagina");
-		goodWords.Add("taco");
-		naughtyWords.Add("Faggot");
-		goodWords.Add("Nerf Herder");
-		naughtyWords.Add("faggot");
-		goodWords.Add("nerf herder");
-		naughtyWords.Add("Fag");
-		goodWords.Add("Cigarette");
-		naughtyWords.Add("fag");
-		goodWords.Add("cigarette");
-		naughtyWords.Add("Dick");
-		goodWords.Add("Doodle");
-		naughtyWords.Add("dick");
-		goodWords.Add("doodle");
-		naughtyWords.Add("Bastard");
-		goodWords.Add("Balloon Head");
-		naughtyWords.Add("bastard");
-		goodWords.Add("balloon head");
-		naughtyWords.Add("Cock");
-		goodWords.Add("Rooster");
-		naughtyWords.Add("cock");
-		goodWords.Add("rooster");
-		naughtyWords.Add("Nigger");
-		goodWords.Add("Nagger");
-		naughtyWords.Add("nigger");
-		goodWords.Add("nagger");
-		naughtyWords.Add("Kike");
-		goodWords.Add("kite");
-		naughtyWords.Add("kike");
-		goodWords.Add("kite");
-		naughtyWords.Add("Chode");
-		goodWords.Add("Dumb Face");
-		naughtyWords.Add("chode");
-		goodWords.Add("dumb face");
-		naughtyWords.Add("Douche");
-		goodWords.Add("Doodad");
-		naughtyWords.Add("douche");
-		goodWords.Add("doodad");
+   		PatternTemplate = @"\b(fuck|ass|shit|bitch|damn|cunt|fag|faggot|dick|bastard|cock|nigger|kike|chode|douche)(s|ing|es)?\b";
+		Options = RegexOptions.IgnoreCase;
+		rx = new Regex(PatternTemplate, Options);
    }
 }
