41f4838172d2a02d9a9e2f49386f37432b4e6aca
baston <baston@ucsc.edu>
2014-06-08 18:01:21 -0700

    Added a dibs sphere to art hall. Added a few dibs models to project. Forgot to add random script for cheering guests.

---
+++ Intrigue/Assets/Scripts/Game/RandomAnim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomAnim : MonoBehaviour {
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.enabled = false;
+		StartCoroutine(StartAnims(Random.Range(1,3)));
+	}
+
+	IEnumerator StartAnims(float time){
+		yield return new WaitForSeconds(time);
+		anim.enabled = true;
+	}
+}
