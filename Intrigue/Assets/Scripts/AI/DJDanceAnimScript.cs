90f3b35bdeb54d2335f9a009ca607733bc41e77d
Matt Scorca <mscorca@ucsc.edu>
2014-06-10 18:17:04 -0700

    Dance changes

---
+++ Intrigue/Assets/Scripts/AI/DJDanceAnimScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DJDanceAnimScript : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.SetBool("DJdance", true);
+	}
+}
