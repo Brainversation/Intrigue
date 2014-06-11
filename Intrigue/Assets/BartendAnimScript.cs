82bc69c2462fbbd5e53ea99c68d1d87abc781be5
Matt Scorca <mscorca@ucsc.edu>
2014-06-10 19:49:24 -0700

    Changes to music hall

---
+++ Intrigue/Assets/BartendAnimScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BartendAnimScript : MonoBehaviour {
+
+	private bool done = true;
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if(done){
+			done = false;
+			StartCoroutine(StartPointing(Random.Range(5,10)));
+		}
+	}
+
+	IEnumerator StartPointing(float time){
+		yield return new WaitForSeconds(time);
+		anim.SetBool("Pointing", true);
+		StartCoroutine(EndPointing(anim.GetCurrentAnimatorStateInfo(0).length));
+	}
+
+	IEnumerator EndPointing(float time){
+		yield return new WaitForSeconds(time);
+		anim.SetBool("Pointing", false);
+		done = true;
+	}
+}
