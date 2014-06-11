d052b13bce1b12a65107a92f48e8f86f682de2f7
Ian Winter <iwinter@ucsc.edu>
2014-06-10 19:41:15 -0700

    strobe

---
+++ Intrigue/Assets/Strobe.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Strobe : MonoBehaviour {
+
+	public float StrobeRate;
+	public Light StrobeLight;
+	public bool DynamicIntensity;
+	private float realStrobe;
+
+	void Start () {
+		StartCoroutine("Strobinator");
+	}
+	
+	IEnumerator Strobinator(){
+		while(true){
+			realStrobe = (1-StrobeRate/100);
+			StrobeLight.enabled = !StrobeLight.enabled;
+			if(DynamicIntensity)
+				StrobeLight.intensity = Random.Range(1,10);
+			yield return new WaitForSeconds(realStrobe);
+		}
+	}
+}
